92fe6df02f8a86f457c1919927efeb94c2685e4d
Gabriella Mara <gabriellamara.p@gmail.com>
2017-11-10 21:43:28 -0200

    Create Day00 solution in Java

---
+++ Java/Day00/Solution0.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+/**
+ * Day00 - "Hello, World" solution
+ * @author Gabriella Mara
+ * @version 1.0
+ */
+
+public class Solution0 {
+	public static void main(String[] args) {
+        
+		Scanner in = new Scanner(System.in); 
+		String inputString = in.nextLine(); 
+    in.close(); 
+      
+		System.out.println("Hello, World.\n" + inputString);
+	}
+}
